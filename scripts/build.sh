fcd0795d5d71b48483c09add0cb35d0ea8c642fe
Ashen Gunaratne <mail@ashenm.ml>
2018-05-06 18:34:18 +0530

    [Skip CI] Add support for custom image tag

--- scripts/build.sh
+++ scripts/build.sh
@@ -2,11 +2,17 @@
 
 set -e
 
-# image tag
-BUILD_TARGET=ashenm/workspace
+# image label
+BUILD_TAG=dev
+BUILD_IMAGE=ashenm/workspace
 
 # scripts directory
 SCRIPT_DIRECTORY="$(dirname "$(readlink -f "$BASH_SOURCE")")"
 
+# handle custom tag
+test "$1" = "-t" \
+  -o "$1" = "--tag" \
+    && BUILD_TAG="${2:?Invalid TAG}"
+
 # build image
-docker build --no-cache --tag "$BUILD_TARGET":dev "$(dirname "$SCRIPT_DIRECTORY")"
+docker build --no-cache --tag "$BUILD_IMAGE:$BUILD_TAG" "$(dirname "$SCRIPT_DIRECTORY")"
