d0902e5239b375fcf68d5e70e6a13d81d0044244
Ashen Gunaratne <mail@ashenm.ml>
2018-05-06 18:13:34 +0530

    [Skip CI] Default remove only ':dev' unless specified

--- scripts/clean.sh
+++ scripts/clean.sh
@@ -2,8 +2,14 @@
 
 set -e
 
-# image tag
+# image name
 BUILD_TARGET=ashenm/workspace
+
+# remove only `dev` tag if
+# not explicitly specified
+test ! "$1" = "-a" \
+  -a ! "$1" = "--all" \
+    && BUILD_TARGET="$BUILD_TARGET:dev"
 
 # remove all `BUILD_TARGET` images
 docker images --all "$BUILD_TARGET" | awk 'NR>1 { print $1":"$2 }' | xargs -r docker rmi
