f5cd282b97d36fa888c2e7698e5162bb1b48278d
Ashen Gunaratne <mail@ashenm.ml>
2019-05-23 10:54:51 +0000

    Add tree shim

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,8 +4,9 @@
 set -e
 
 # clinch permissions
-find filesystem/etc -type d -exec chmod 755 {} \;
+find filesystem -type d -exec chmod 755 {} \;
 find filesystem/etc -type f -exec chmod 644 {} \;
 find filesystem/etc/sudoers.d -type d -exec chmod 750 {} \;
 find filesystem/etc/profile.d -type f -exec chmod 755 {} \;
 find filesystem/etc/sudoers.d -type f -exec chmod 440 {} \;
+find filesystem/usr/local/bin -type f -exec chmod 755 {} \;
