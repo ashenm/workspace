0b784c1ae8d147c8aa3a72def257545594ae37dc
Ashen Gunaratne <mail@ashenm.ml>
2019-06-06 16:20:19 +0530

    Add ashenm/shell build trigger

--- scripts/culminate.sh
+++ scripts/culminate.sh
@@ -12,3 +12,10 @@ curl --fail --silent --show-error --request POST --output /dev/null \
   --header "Travis-API-Version: 3" --header "Authorization: token $TRAVIS_API_TOKEN" \
   --data "{ \"request\": { \"branch\": \"latest\", \"message\": \"Build to parity with commit "$TRAVIS_COMMIT" on "$TRAVIS_REPO_SLUG"\" } }" \
   --url "https://api.travis-ci.org/repo/ashenm%2Fxworkspace/requests"
+
+# ashenm/shell
+curl --fail --silent --show-error --request POST --output /dev/null \
+  --header "Content-Type: application/json" --header "Accept: application/json" \
+  --header "Travis-API-Version: 3" --header "Authorization: token $TRAVIS_PRO_API_TOKEN" \
+  --data "{ \"request\": { \"branch\": \"latest\", \"message\": \"Build to parity with commit "$TRAVIS_COMMIT" on "$TRAVIS_REPO_SLUG"\" } }" \
+  --url "https://api.travis-ci.com/repo/ashenm%2Fshell/requests"
