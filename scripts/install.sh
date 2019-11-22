e61af0351a81b07e892855c7747d877625445b2e
Ashen Gunaratne <mail@ashenm.ml>
2019-11-22 13:37:17 +0000

    Replace brace expansion with static corresponding expansions

--- scripts/install.sh
+++ scripts/install.sh
@@ -14,4 +14,5 @@ find filesystem/etc -type f -exec chmod 644 {} \;
 find filesystem/etc/sudoers.d -type d -exec chmod 750 {} \;
 find filesystem/etc/profile.d -type f -exec chmod 755 {} \;
 find filesystem/etc/sudoers.d -type f -exec chmod 440 {} \;
-find filesystem/usr/local/{bin,sbin} -type f -exec chmod 755 {} \;
+find filesystem/usr/local/sbin -type f -exec chmod 755 {} \;
+find filesystem/usr/local/bin -type f -exec chmod 755 {} \;
