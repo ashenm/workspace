2d9d0f95ca1277f85177daacea45c14bff9166ae
Ashen Gunaratne <mail@ashenm.ml>
2019-12-08 06:00:28 +0000

    Remove wanton permission clinch

--- scripts/install.sh
+++ scripts/install.sh
@@ -16,7 +16,6 @@ curl --silent --show-error --location --output filesystem/etc/skel/.eslintrc.jso
 find filesystem -type d -exec chmod 755 {} \;
 find filesystem/etc -type f -exec chmod 644 {} \;
 find filesystem/etc/sudoers.d -type d -exec chmod 750 {} \;
-find filesystem/etc/profile.d -type f -exec chmod 755 {} \;
 find filesystem/etc/sudoers.d -type f -exec chmod 440 {} \;
 find filesystem/usr/local/sbin -type f -exec chmod 755 {} \;
 find filesystem/usr/local/bin -type f -exec chmod 755 {} \;
