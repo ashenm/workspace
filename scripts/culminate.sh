a98e029bdf361b0a4b1d8c9a72dfc938cadc2eb6
Ashen Gunaratne <mail@ashenm.ml>
2019-12-31 07:42:31 +0000

    [Skip CI] Add tag stanford-cs143 trigger

--- scripts/culminate.sh
+++ scripts/culminate.sh
@@ -6,6 +6,13 @@ set -e
 # truncate commit hash
 TRAVIS_COMMIT=`expr substr "$TRAVIS_COMMIT" 1 7`
 
+# ashenm/workspace:stanford-cs143
+curl --fail --silent --show-error --request POST --output /dev/null \
+  --header "Content-Type: application/json" --header "Accept: application/json" \
+  --header "Travis-API-Version: 3" --header "Authorization: token $TRAVIS_API_TOKEN" \
+  --data "{ \"request\": { \"branch\": \"stanford-cs143\", \"message\": \"Build to parity with commit "$TRAVIS_COMMIT" on "$TRAVIS_REPO_SLUG"\" } }" \
+  --url "https://api.travis-ci.org/repo/ashenm%2Fworkspace/requests"
+
 # ashenm/xworkspace
 curl --fail --silent --show-error --request POST --output /dev/null \
   --header "Content-Type: application/json" --header "Accept: application/json" \
