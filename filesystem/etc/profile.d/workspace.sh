0dcfdd80c18ba3d8b49838127f5625e96dc433ee
Ashen Gunaratne <mail@ashenm.ml>
2020-01-17 03:31:17 +0000

    Transfer common profile.d script excerpts to ashenm/environment

--- filesystem/etc/profile.d/workspace.sh
+++ filesystem/etc/profile.d/workspace.sh
@@ -1,16 +1,19 @@
+##################################################
+#
+# Workspace Shell Initialisation
+# https://github.com/ashenm/workspace
+#
+# Ashen Gunaratne
+# mail@ashenm.ml
+#
+##################################################
+
 # if not root
 if [ `id -u` -ne 0 ]
 then
 
   # hub
   alias git='hub'
-
-  # gnu
-  alias time='command time -v'
-
-  # python
-  alias pip='pip3'
-  alias python='python3'
 
   # asciidoctor
   alias asciidoctor-pdf='asciidoctor-pdf --require asciidoctor-mathematical --attribute mathematical-format=svg'
