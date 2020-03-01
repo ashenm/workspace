3fe471f054963c66c937e5af10c5000b586e2210
Ashen Gunaratne <mail@ashenm.ml>
2020-03-01 18:50:24 +0530

    Configure OpenJDK assessments

--- tests/config.sh
+++ tests/config.sh
@@ -4,6 +4,9 @@ set -e
 
 imageTests+=(
   [ashenm/workspace]='
+    java-hello-world
+    java-ca-certificates
+    java-uimanager-font
     node-hello-world
     perl-hello-world
     ruby-hello-world
